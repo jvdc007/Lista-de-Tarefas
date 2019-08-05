475e755e6b6b37e1395bf6c458183137a7b89e5d
Joao Vitor de Medeiros <you@example.com>
2019-08-05 16:36:20 -0300

    Incremento de exclusão de itens e armazenamento no local storage

--- list.js
+++ list.js
@@ -1,7 +1,7 @@
 let lista = document.querySelector('ul');
 let tarefa = document.querySelector('input#texto');
 let botao = document.querySelector('input#botao');
-let listaTarefas = [];
+let listaTarefas = JSON.parse(localStorage.getItem('lista')) || [];
 
 renderLista();
 
@@ -10,14 +10,36 @@ function renderLista(){
     for(list of listaTarefas){
         let item = document.createElement('li');
         let texto = document.createTextNode(list);
+        let excluir = document.createElement('a');
+        let pos = listaTarefas.indexOf(list);
+        excluir.innerText = "Excluir";
+        excluir.href = "#";
+        excluir.setAttribute('onclick', 'removerTarefa('+pos+')');
         item.appendChild(texto);
+        item.appendChild(excluir);
         lista.appendChild(item);
     }
 }
 
 function addTarefa(){
+    tarefa.focus();
     let item = tarefa.value;
-    listaTarefas.push(item);
-    tarefa.value = '';
+    if(item == ""){
+        return;
+    }else{
+        listaTarefas.push(item);
+        tarefa.value = '';
+        renderLista();
+        save();
+    }
+}
+
+function removerTarefa(pos){
+    listaTarefas.splice(pos, 1);
     renderLista();
+    save();
+}
+
+function save(){
+    localStorage.setItem('lista', JSON.stringify(listaTarefas));
 }
